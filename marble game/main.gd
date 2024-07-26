1ba4bcb7fc54c465e2c5b4a0422cc59619b054cf
trand21-school <trand21@student.cbhs.school.nz>
2024-07-26 12:19:02 +1200

    gk

--- marble game/main.gd
+++ marble game/main.gd
@@ -3,14 +3,11 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Startscreen/diescreen.hide()
-	$Startscreen/success.hide()
-
+	get_tree().change_scene_to_file("res://start_screen.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func _on_start_button_up():
-	pass # Replace with function body.
+
--- marble game/star_screen.gd
+++ marble game/star_screen.gd
@@ -12,9 +12,8 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	
 	get_tree().change_scene_to_file("res://lvl1.tscn")
-
+	
 
 func _on_levels_pressed():
 	get_tree().change_scene_to_file("res://lvl_select.tscn")
