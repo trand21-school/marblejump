68a45cc13cc5aeff5f3c2e1dc500785ca0b99ea7
trand21-school <trand21@student.cbhs.school.nz>
2024-08-02 11:25:21 +1200

    rf

--- marble game/lvl3.gd
+++ marble game/lvl3.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.currentlevel=2
+	Global.currentlevel=3
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +15,5 @@ func die():
 	get_tree().change_scene_to_file("res://diescreen.tscn")
 
 func _on_death_body_entered(body):
-	if is_in_group("Player"):
-		print("yur")
+	if is_in_group("Playe"):
 		die()
